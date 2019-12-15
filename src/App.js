af057992407ce4d5a41a25f9311f1f67b689d578
giulia-code <giuliacatalano@me.com>
2019-12-15 12:31:53 +0100

    progress on the todos component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Todos from './Todos';
 
-function App() {
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some mill'},
+      {id: 2, content: 'play mario kart'}
+    ]
+  }
+  render() {
   return (
     <div className="App">
-      
+      <h1 className="center blue-text"> Todo's</h1>
+      <Todos todos={this.state.todos} />
     </div>
   );
+ }
 }
 
 export default App;
